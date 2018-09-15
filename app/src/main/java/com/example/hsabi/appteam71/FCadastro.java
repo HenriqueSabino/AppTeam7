a2bfb38eaf399025d15f6fd1c178612e73050076
HenriqueSabino <h.sabinocosta@gmail.com>
2018-09-14 21:12:40 -0300

    Save1

--- app/src/main/java/com/example/hsabi/appteam71/FCadastro.java
+++ app/src/main/java/com/example/hsabi/appteam71/FCadastro.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.*;
 
-public class FCadastro extends Fragment{
+public class FCadastro extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState) {
         return inflater.inflate(R.layout.f_cadastro, container, false);
     }
 }

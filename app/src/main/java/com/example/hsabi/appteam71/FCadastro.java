ac68f3ef7990938216d690b3e3e9dbd7160f8a8a
HenriqueSabino <h.sabinocosta@gmail.com>
2018-09-24 14:57:13 -0300

    Serialização Feita com secesso

--- app/src/main/java/com/example/hsabi/appteam71/FCadastro.java
+++ app/src/main/java/com/example/hsabi/appteam71/FCadastro.java
@@ -5,30 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class FCadastro extends AppCompatActivity {
 
-    private Spinner sp;
-    private String[] areasInteresse;
-    private String[] competencias;
+    private Spinner sp_area;
+    private Spinner sp_competencia;
 
     @Override
     protected void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
         setContentView(R.layout.f_cadastro);
 
-        sp = (Spinner) findViewById(R.id.spn_areaInteresse);
+        sp_area = (Spinner) findViewById(R.id.spn_areaInteresse);
+        sp_competencia = (Spinner) findViewById(R.id.spn_competencias);
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.list_areaInteresse, android.R.layout.simple_spinner_item);
-        sp.setAdapter(adapter);
+        sp_area.setAdapter(adapter);
+
+        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,R.array.list_competencia, android.R.layout.simple_spinner_item);
+        sp_competencia.setAdapter(adapter2);
 
         AdapterView.OnItemSelectedListener choice = new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String item = sp.getSelectedItem().toString();
-                System.out.println("item: " + item);
             }
 
             @Override
@@ -36,10 +40,39 @@ public class FCadastro extends AppCompatActivity {
 
             }
         };
-        sp.setOnItemSelectedListener(choice);
+        sp_area.setOnItemSelectedListener(choice);
+
+        AdapterView.OnItemSelectedListener choice2 = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        sp_competencia.setOnItemSelectedListener(choice2);
     }
 
     public void OnClick(View view){
-        System.out.println("Conta Criada");
+        boolean test = false;
+        try {
+            AccountsManager manager = new AccountsManager(getFilesDir(), getApplicationContext());
+            String userName = ((EditText)findViewById(R.id.nomeCompleto)).getText().toString();
+            String email = ((EditText)findViewById(R.id.email)).getText().toString();
+            String senha = ((EditText)findViewById(R.id.senha)).getText().toString();
+            String area = ((Spinner)findViewById(R.id.spn_areaInteresse)).getSelectedItem().toString();
+            String competencia = ((Spinner)findViewById(R.id.spn_competencias)).getSelectedItem().toString();
+
+            manager.Cadastrar(userName, senha, email, area, competencia);
+        }
+        catch (IOException e){
+            Toast.makeText(getApplicationContext(),"IOException: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        catch (ClassNotFoundException e){
+            Toast.makeText(getApplicationContext(),"ClassNotFoundException: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/hsabi/appteam71/MainActivity.java
+++ app/src/main/java/com/example/hsabi/appteam71/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -16,10 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-
     public void CriarConta(View view){
         Intent i = new Intent(getApplicationContext(), FCadastro.class);
         startActivity(i);
+    }
+
+    public void Entrar (View view){
+        try{
+            AccountsManager manager = new AccountsManager(getFilesDir(), getApplicationContext());
+            String email = ((EditText) findViewById(R.id.txt_email)).getText().toString();
+            String senha = ((EditText) findViewById(R.id.txt_senha)).getText().toString();
+
+            User temp = manager.FindByEmail(email);
+            if (temp.CheckLogin(email, senha)){
+                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Erouuu!!!", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (Exception e){
+            Toast.makeText(getApplicationContext(), "ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
